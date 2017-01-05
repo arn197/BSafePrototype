22a2fff238e869f4c8b3bd18df2307269f734f68
arn197 <aaronjacob00@gmail.com>
2017-01-05 18:06:48 +0530

    Good stuff

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+var db;
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://arn197:hellobyebye@ds155028.mlab.com:55028/bsafe',function(err, database){     
+	if(err)         
+		return console.log("Error while connecting to database");     
+	db = database; 
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/view',function(req,res){
+  res.sendFile(__dirname + '/views/heatmap.html');
+})
+
+app.get('/views',function(req,res){
+	db.collection('latlng').find().toArray(function(err,result){         
+		console.log(result);         
+		res.json(result);     
+	});
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
