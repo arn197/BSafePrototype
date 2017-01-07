f2e3cb5143090f1aa7e4c8a3dc321e5b1c2a7d57
arn197 <aaronjacob00@gmail.com>
2017-01-08 03:51:06 +0530

    Im tired

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var app = express();
+var request = require('request');
 
+var app = express();
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 var db;
 var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://arn197:hellobyebye@ds155028.mlab.com:55028/bsafe',function(err, database){     
@@ -21,6 +24,18 @@ app.get('/views',function(req,res){
 	db.collection('latlng').find().toArray(function(err,result){         
 		console.log(result);         
 		res.json(result);     
+	});
+})
+
+app.get('/sendata',function(req,res){
+	var data;
+	var id = req.body.id;
+	var token = req.body.tok;
+	request('http://graph.facebook.com/' + id, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body); // Show the HTML for the Modulus homepage.
+        data = body;
+    }
 	});
 })
 
